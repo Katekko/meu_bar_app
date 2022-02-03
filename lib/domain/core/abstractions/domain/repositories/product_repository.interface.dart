4a9f5b9182fb1ee94050b11d2d4800ae7cf5cab3
Katekko <joaovixgon@gmail.com>
2022-02-03 18:41:02 -0300

    Criando interface para product repository

---
+++ lib/domain/core/abstractions/domain/repositories/product_repository.interface.dart
@@ -0,0 +1,31 @@
+import 'package:ekko/domain/product/models/category.model.dart';
+import 'package:ekko/domain/product/models/product.model.dart';
+
+abstract class IProductRepository {
+  /// Pegar os produtos existentes com a possibilidade de filtrar
+  /// pelas categorias usando o atributo [categories] ou [filter]
+  Future<List<ProductModel>> getProducts({
+    String filter,
+    List<CategoryModel> categories,
+  });
+
+  Future<ProductModel> getProductById(int id);
+
+  Future<void> registerProduct(ProductModel product);
+
+  Future<void> updateProduct(ProductModel product);
+
+  Future<void> deleteProduct(ProductModel product);
+
+  /// Pegar as categorias existentes com a possibilidade de filtrar
+  /// usando o atributo [filter]
+  Future<List<CategoryModel>> getCategories({String filter});
+
+  Future<CategoryModel> getCategoryById(int id);
+
+  Future<void> registerCategory(CategoryModel category);
+
+  Future<void> updateCategory(CategoryModel category);
+
+  Future<void> deleteCategory(CategoryModel category);
+}
