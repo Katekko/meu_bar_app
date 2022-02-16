a44ef5dd7eff20b89f91d33a4c6d28401948cafc
Katekko <joaovixgon@gmail.com>
2022-02-16 10:48:36 -0300

    Test update category

--- test/presentation/categories/controllers/category_controller_test.dart
+++ test/presentation/categories/controllers/category_controller_test.dart
@@ -2,7 +2,6 @@ import 'package:ekko/domain/core/abstractions/domain/repositories/product_reposi
 import 'package:ekko/domain/core/abstractions/presentation/controllers/categories/category_controller.interface.dart';
 import 'package:ekko/domain/core/abstractions/presentation/field.interface.dart';
 import 'package:ekko/domain/product/models/category.model.dart';
-import 'package:ekko/domain/product/product_mock.repository.dart';
 import 'package:ekko/presentation/categories/controllers/category.controller.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
@@ -10,14 +9,16 @@ import 'package:test/test.dart';
 import '../../../mocks.dart';
 
 void main() {
-  late final ICategoryController controller;
+  late ICategoryController controller;
   late final IProductRepository productRepository;
   late final IField nameField;
 
   setUpAll(() {
     productRepository = ProductRepositoryMock();
     nameField = FieldMock();
+  });
 
+  setUp(() {
     controller = CategoryController(
       productRepository: productRepository,
       loading: LoadingControllerMock(),
@@ -33,7 +34,6 @@ void main() {
   });
 
   test('should create a category with success', () async {
-    // arrange
     const name = 'katekko';
     when(() => nameField.validate()).thenReturn(true);
     when(() => nameField.value).thenReturn(name);
@@ -51,10 +51,45 @@ void main() {
     when(() => productRepository.registerCategory(category))
         .thenAnswer((_) async {});
 
+    await controller.saveCategory(backScreen: () {});
+
+    verify(() => productRepository.registerCategory(category));
+  });
+
+  test('should update a category with success', () async {
+    // arrange
+    const name = 'katekko';
+    when(() => nameField.validate()).thenReturn(true);
+    when(() => nameField.value).thenReturn(name);
+    when(() => nameField.hasError).thenReturn(false);
+
+    const category = CategoryModel(id: 1, name: 'Katekko', icon: 1);
+
+    final controller = CategoryController(
+      productRepository: productRepository,
+      loading: LoadingControllerMock(),
+      isEdit: true,
+      nameField: nameField,
+      category: category,
+    );
+
+    controller.pickAnIcon(2);
+    final icon = await controller.iconFieldStream.first;
+    controller.nameField.value = 'katekko 2';
+
+    final categoryToSave = CategoryModel(
+      id: 1,
+      name: controller.nameField.value,
+      icon: icon!,
+    );
+
+    when(() => productRepository.updateCategory(categoryToSave))
+        .thenAnswer((_) async {});
+
     // action
     await controller.saveCategory(backScreen: () {});
 
     // assert
-    verify(() => productRepository.registerCategory(category));
+    verify(() => productRepository.updateCategory(categoryToSave));
   });
 }
