74e5050d86cd861568f64720440f879b23ed623d
Katekko <joaovixgon@gmail.com>
2022-02-23 09:43:00 -0300

    Testando classe required field validator com os novos fluxos

--- lib/domain/core/validators/required_field.validator.dart
+++ lib/domain/core/validators/required_field.validator.dart
@@ -8,10 +8,11 @@ class RequiredFieldValidator<T> extends Equatable
   String? validate(T? value) {
     if (value == null) return ValidatorsTranslate.requiredField;
 
-    if (value is String) {
-      return value.isEmpty ? ValidatorsTranslate.requiredField : null;
-    } else if (value is int || value is double || value is num) {
-      return value != 0 ? null : ValidatorsTranslate.requiredField;
+    if (value is String && value.isEmpty) {
+      return ValidatorsTranslate.requiredField;
+    } else if ((value is int || value is double || value is num) &&
+        (value == 0 || value == .0)) {
+      return ValidatorsTranslate.requiredField;
     }
 
     return null;
--- test/domain/validators/required_field_validator_test.dart
+++ test/domain/validators/required_field_validator_test.dart
@@ -1,5 +1,6 @@
 import 'package:ekko/domain/core/validators/required_field.validator.dart';
 import 'package:ekko/infrastructure/translate/validators.translate.dart';
+import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -9,15 +10,23 @@ void main() {
     sut = RequiredFieldValidator();
   });
 
+  test('Should return error if value is null', () {
+    expect(sut.validate(null), ValidatorsTranslate.requiredField);
+  });
+
   test('Should return null if value is not empty', () {
-    expect(sut.validate('any_value'), null);
+    expect(sut.validate(faker.lorem.random.string(10)), null);
+  });
+
+  test('Should return null if value is a number greater then zero', () {
+    expect(sut.validate(faker.currency.random.integer(20, min: 1)), null);
   });
 
   test('Should return error if value is empty', () {
     expect(sut.validate(''), ValidatorsTranslate.requiredField);
   });
 
-  test('Should return error if value is null', () {
-    expect(sut.validate(null), ValidatorsTranslate.requiredField);
+  test('Should return error if value is zero', () {
+    expect(sut.validate(0), ValidatorsTranslate.requiredField);
   });
 }
