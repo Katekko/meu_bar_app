b2bc4133a69bff1e462a825556b58f7635610325
Katekko <joaovixgon@gmail.com>
2022-02-23 11:32:54 -0300

    Criando text fields na tela de criar o produto

--- lib/presentation/products/screens/product.screen.dart
+++ lib/presentation/products/screens/product.screen.dart
@@ -2,15 +2,38 @@ import 'package:ekko/domain/core/abstractions/presentation/controllers/products/
 import 'package:ekko/presentation/shared/view_controller.interface.dart';
 import 'package:flutter/material.dart';
 
+import '../../shared/text_field.widget.dart';
+
 class ProductScreen extends ViewController<IProductController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ProductScreen'),
-        centerTitle: true,
+        title: Text(
+          controller.isEdit ? 'Atualizando um Produto' : 'Criando um Produto',
+        ),
       ),
-      body: const Center(),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            TextFieldWidget(
+              label: 'Nome do Produto',
+              field: controller.nameField,
+            ),
+            const SizedBox(height: 20),
+            TextFieldWidget(
+              label: 'Descrição',
+              field: controller.descriptionField,
+            ),
+            const SizedBox(height: 20),
+            TextFieldWidget(
+              label: 'Quanto custa?',
+              field: controller.priceField,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
