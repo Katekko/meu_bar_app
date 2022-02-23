1108b0d8f6d60c49be7b18090d4847e93befbe63
Katekko <joaovixgon@gmail.com>
2022-02-23 16:23:58 -0300

    Criando label para campo de categorio

--- lib/presentation/products/widgets/product/category.widget.dart
+++ lib/presentation/products/widgets/product/category.widget.dart
@@ -25,70 +25,81 @@ class CategoryWidget extends ViewController<IProductController> {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<CategoryModel?>(
-      stream: controller.categoryField.stream,
-      builder: (_, snap) {
-        return InkWell(
-          onTap: () => openCategoriesSearch(context),
-          child: SizedBox(
-            height: 50,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(3),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Escolha a categoria do produto:',
+          style: Theme.of(context).textTheme.subtitle1,
+        ),
+        const SizedBox(height: 5),
+        StreamBuilder<CategoryModel?>(
+          stream: controller.categoryField.stream,
+          builder: (_, snap) {
+            return InkWell(
+              onTap: () => openCategoriesSearch(context),
+              child: SizedBox(
+                height: 50,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        child: Ink(
+                          child: snap.hasData
+                              ? Icon(
+                                  IconData(
+                                    snap.data!.icon,
+                                    fontFamily: 'MaterialIcons',
+                                  ),
+                                )
+                              : const SizedBox(),
+                        ),
+                      ),
                     ),
-                    child: Ink(
-                      child: snap.hasData
-                          ? Icon(
-                              IconData(
-                                snap.data!.icon,
-                                fontFamily: 'MaterialIcons',
-                              ),
-                            )
-                          : const SizedBox(),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        child: Ink(
+                          child: snap.hasData
+                              ? Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Text(
+                                      snap.data!.name,
+                                      textAlign: TextAlign.start,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .subtitle1!
+                                          .copyWith(
+                                            color: Colors.black87,
+                                            fontWeight: FontWeight.w400,
+                                          ),
+                                    ),
+                                  ),
+                                )
+                              : const SizedBox(),
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  flex: 4,
-                  child: Container(
-                    height: double.infinity,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(3),
-                    ),
-                    child: Ink(
-                      child: snap.hasData
-                          ? Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Text(
-                                  snap.data!.name,
-                                  textAlign: TextAlign.start,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .subtitle1!
-                                      .copyWith(
-                                          color: Colors.black87,
-                                          fontWeight: FontWeight.w400),
-                                ),
-                              ),
-                            )
-                          : const SizedBox(),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
